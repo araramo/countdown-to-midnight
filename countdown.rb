186d2118fe32c1176a323f2919ef68388876bbf4
Araceli Ramos <celi2455@gmail.com>
2021-09-18 16:44:47 -0500

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
+number = 10
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+count = 5
+
+def countdown_with_sleep(count)
+    while count > 0
+        puts "#{count} SECOND(S)!"
+        sleep(1.5)
+        count -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
